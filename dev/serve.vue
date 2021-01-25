30e1a1a9aca9ef007656fbca32abb643bed5521c
LeonardRoman <leonardroman1@gmail.com>
2021-01-25 15:42:09 +0300

    git init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import Layout from '@/layout.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    Layout
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <layout />
+  </div>
+</template>
